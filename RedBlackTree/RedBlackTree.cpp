9d4823dd0985bb97f2240f94aa119182f6653343
OkunElya <okun.elya@gmail.com>
2024-12-15 12:33:33 +1000

    added rotations and 'add' method to the tree class

--- RedBlackTree/RedBlackTree.cpp
+++ RedBlackTree/RedBlackTree.cpp
@@ -41,24 +41,148 @@ class RedBlackTree {
     };
 
     struct node {
-        struct node* left;
-        struct node* right;
-        struct node* parent;
+        struct node* left=nullptr;
+        struct node* right=nullptr;
+        struct node* parent=nullptr;
         struct dataStruct data;
-        enum Color;
-
+        enum Color color=red;
+		//some 'safe' to use functions
+        node* gran() {
+			if (this->parent) {
+				return this->parent->parent;
+			}
+			return nullptr;
+        }
+		node* brother() {
+			if (this->parent) {
+				if (this == this->parent->left)
+					return this->parent->right;
+				else
+					return this->parent->left;
+			}
+			return nullptr;
+		}
+        node* uncle() {
+			if (this->parent) {
+				return this->parent->brother();
+			}
+			return nullptr;
+        }
     };
 
-    struct node* parent=nullptr;
+	void rotateLeft(node* toRot) {
+        if (!toRot || !toRot->right) {//sanity check
+            std::cerr << "Rotating node that shouldn't be rotated\n";
+            return;  // Cannot rotate if node is null or has no right child
+        }
+        //parent handling
+        inline node* newParent = toRot->right;//better readability
+        if (toRot->parent) {
+            if (toRot->parent->left == toRot) {
+                toRot->parent->left = newParent; //because after the left turn the right node will be tha parent of the left    
+            }
+            else {
+                toRot->parent->right = newParent;//same as before 
+            }
+            newParent->parent = toRot->parent;
+        }
+        else {
+            //rotated node is the root
+            root = newParent;
+            newParent->parent = nullptr;
+        }
+
+        //the roataion part itself
+        node* buf = newParent->left;//the one that'll be unassigned, so wee need to remember it
+        newParent->left = toRot;//make rot a left child of it's right node
+        
+        toRot->parent = newParent;//make node of the right of the rot it's parent
+        newParent = buf;
+        if (buf) {
+            buf->parent = toRot;
+        }
+	}
+    void rotateRight(node* toRot) {
+        if (!toRot || !toRot->left) {//sanity check
+            std::cerr << "Rotating node that shouldn't be rotated\n";
+            return;  // Cannot rotate if node is null or has no right child
+        }
+        //parent handling
+        inline node* newParent = toRot->left;//better readability
+        if (toRot->parent) {
+            if (toRot->parent->left == toRot) {
+                toRot->parent->left = newParent; //because after the right turn the left node will be tha parent of the left    
+            }
+            else {
+                toRot->parent->right = newParent;//same as before 
+            }
+            newParent->parent = toRot->parent;
+        }
+        else {
+            //rotated node is the root
+            root = newParent;
+            newParent->parent = nullptr;
+        }
+
+        //the roataion part itself
+        node* buf = newParent->right;//the one that'll be unassigned, so wee need to remember it
+        newParent->right = toRot;//make rot a left child of it's right node
+
+        toRot->parent = newParent;//make node of the right of the rot it's parent
+        newParent = buf;
+        if (buf) {
+            buf->parent = toRot;
+        }
+    }
+
+    struct node* root=nullptr;
+    void add(dataStruct item) {
+        //first do simple binary tree insertion
+        node* toInsert = new node;
+        toInsert->data = item;
+
+
+        struct node* temp = root;
+        if (!root) {
+            root = toInsert;
+        }
+        else {
+            while (temp) {
+                if (item > temp->data) {
+                    if (!temp->left)//
+                    {
+                        toInsert->parent = temp;
+                        temp->left = toInsert;
+                        temp = toInsert;//go to the new node
+                        break;
+                    }
+                    else
+                        temp = temp->left;
+                }
+                else {
+                    if (!temp->right)//nullptrCheck    
+                    {
+                        toInsert->parent = temp;
+                        temp->right = toInsert;
+                        temp = toInsert;//go to the new node
+                        break;
+                    }
+                    else
+                        temp = temp->right;
+                }
+
+            }
+        }
+		//we-re standing on a new child node and it's color is red
+    
+    
+    }
 
     bool find(dataStruct value) {
-        struct node* temp = parent;
-        if (!temp)
-            return false;//nullptrCheck
+        struct node* temp = root;
+        
         while (temp) {
             if (value > temp->data) {
-                if (value == temp->data)
-                    return true;
                 if (!temp->left)//nullptrCheck
                     return false;
                 temp = temp->left;
@@ -72,6 +196,7 @@ class RedBlackTree {
                 return true;//found
             }
         }
+        return false;
     }
 };
 
